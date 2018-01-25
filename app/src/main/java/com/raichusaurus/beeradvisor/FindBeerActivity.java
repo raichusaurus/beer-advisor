bb502c78c24fd8b45b1d9534f3c8c8f34b35eeec
felixerus <felixerus@gmail.com>
2018-01-25 15:45:45 -0800

    Have button take selected spinner option and display it in the text view

--- app/src/main/java/com/raichusaurus/beeradvisor/FindBeerActivity.java
+++ app/src/main/java/com/raichusaurus/beeradvisor/FindBeerActivity.java
@@ -3,6 +3,7 @@ package com.raichusaurus.beeradvisor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
@@ -15,7 +16,16 @@ public class FindBeerActivity extends AppCompatActivity {
 
     // call when the user clicks the button
     public void onClickFindBeer(View view) {
+
+        // get a reference to the spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        // get a reference to the text view
         TextView brands = (TextView) findViewById(R.id.brands);
-        brands.setText("Gottle of geer");
+
+        // get the selected item in the spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        brands.setText(beerType);
     }
 }
