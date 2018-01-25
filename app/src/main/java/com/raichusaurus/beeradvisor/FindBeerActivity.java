3ad74809f18d54ec59806023c74735e135d27068
felixerus <felixerus@gmail.com>
2018-01-25 15:37:59 -0800

    Have button call onClickFindBeer() method(skeleton created)

--- app/src/main/java/com/raichusaurus/beeradvisor/FindBeerActivity.java
+++ app/src/main/java/com/raichusaurus/beeradvisor/FindBeerActivity.java
@@ -2,6 +2,7 @@ package com.raichusaurus.beeradvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // call when the user clicks the button
+    public void onClickFindBeer(View view) {
+
     }
 }
